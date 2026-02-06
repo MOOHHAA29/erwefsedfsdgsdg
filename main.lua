e33efc1750f4a57f101119c6ca19633cce31ab78
MOOHHAA29 <165245415+MOOHHAA29@users.noreply.github.com>
2026-02-06 08:39:44 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,25 @@
+
+while true do
+	firesignal(game:GetService("ReplicatedStorage").__ReplicatorInternal.RemoteEvent.OnClientEvent, { { "", "￻", {
+		[game:GetService("HttpService"):GenerateGUID(true) --[[{743bd73a-497d-4517-94c2-18f9619150af}]]] = {
+			data = {
+				Stands = {
+					["5"] = {
+						money = "0;0"
+					}
+				}
+			}
+		}
+	} }, { "", "￴", {
+			Money = "0;54757645.1",
+			PlotData = {
+				Stands = {
+					["5"] = {
+						money = "0;0"
+					}
+				}
+			}
+		} } })
+
+	wait(0)
+end
